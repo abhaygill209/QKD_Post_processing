e3e311180f5784bf7acd77b01722a2d89730b912
Garima Pant <146510408+gr8-devfreak@users.noreply.github.com>
2026-03-25 21:14:00 +0530

    Created FromMont.v

---
+++ HDL_codes/FromMont.v
@@ -0,0 +1,31 @@
+
+module FromMontgomeryConverter #(
+    parameter Q_WIDTH = 14,
+    parameter Q = 12289
+)(
+    input clk,
+    input rst_n,
+    input [Q_WIDTH-1:0] c_bar,
+    input valid_in,
+    output wire valid_out,
+    output wire [Q_WIDTH-1:0] c
+);
+
+    wire [Q_WIDTH-1:0] c_wire;
+
+    MontgomeryReductionUnit #(
+        .Q_WIDTH(Q_WIDTH),
+        .Q(Q)
+    ) mont_inv (
+        .clk(clk),
+        .rst_n(rst_n),
+       .valid_in(valid_in),
+        .b_bar(c_bar),
+      .w_bar({{Q_WIDTH-1{1'b0}},1'b1}),
+      .valid_out(valid_out),
+        .c_bar(c_wire)
+    );
+
+    assign c = c_wire;
+
+endmodule
